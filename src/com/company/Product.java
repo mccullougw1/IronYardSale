28770fff04a08751c5f212bc63ea780c209eaaf5
jtumms <john.tumminelli@tummsmedia.com>
2016-10-20 17:54:30 -0400

    corrected null pointer in addProduct method

--- src/com/company/Product.java
+++ src/com/company/Product.java
@@ -54,7 +54,7 @@ public class Product {
     }
 
     public void setDescription(String description) {
-        description = description;
+        this.description = description;
     }
 
     public double getPrice() {
