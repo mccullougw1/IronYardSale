2d94ed656c5cbb020c0a51b4bcfa9a1996a061ff
William McCullough <williammccullough@Williams-MacBook-Air.local>
2016-11-28 22:16:27 -0500

    need to pull

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,59 @@
 console.log('we ready')
 
-$.getJSON('/get-products', function(data){
+$.getJSON('/get-product', function(data){
    console.log(data)
 })
+
+
+
+
+
+
+
+// {"category":"assorted stuff","dateAdded":"2016-12-01","description":"nothing much to say here","name":"test2","originatorId":2,"price":3400.0,"productId":2}
+
+
+// var dummyData = [
+//    {
+//       category: 'pants',
+//       dateAdded: '2016-09-07',
+//       description: 'im a pair of pants',
+//       name: 'skinny jeans',
+//       originatorId: 17,
+//       price: 49.99,
+//       productId: 1
+//    },
+//    {
+//       category: 'shirt',
+//       dateAdded: '2016-06-27',
+//       description: 'long-sleeve plaid',
+//       name: 'hipster plaid',
+//       originatorId: 24,
+//       price: 27.50,
+//       productId: 2
+//    }
+// ]
+//
+//
+//
+// var sendBloatData = {
+//    username: "thisBigMan",
+//    password: "testtest"
+// }
+
+var homePage = function(){
+   
+}
+
+
+var routerController = function(){
+   var currentHash = window.location.hash.slice(1)
+
+   if(currentHash.length === 0) {
+      return homePage()
+   }
+
+}
+
+window.addEventListener('hashchange', routerController)
+routerController()
